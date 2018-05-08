dbd9f74572dc6e72de9cfd20e795eac77fe98420
Rakshit Vats <rakshitvats@ymail.com>
2018-05-08 17:19:48 +0530

    Lyrics
    added initial support for lyrics

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -42,6 +42,9 @@ from .json import Json
 from .constants import VERSION as BOTVERSION
 from .constants import DISCORD_MSG_CHAR_LIMIT, AUDIO_CACHE_PATH
 
+import re
+import urllib.request
+from bs4 import BeautifulSoup
 
 load_opus_lib()
 
@@ -3060,3 +3063,30 @@ class MusicBot(discord.Client):
             log.debug("Pausing player in \"{}\" due to unavailability.".format(server.name))
             self.server_specific_data[server]['availability_paused'] = True
             player.pause()
+
+    async def cmd_lyrics(self, artist, song_title):
+        artist = artist.lower()
+        song_title = song_title.lower()
+        # remove all except alphanumeric characters from artist and song_title
+        artist = re.sub('[^A-Za-z0-9]+', "", artist)
+        song_title = re.sub('[^A-Za-z0-9]+', "", song_title)
+        if artist.startswith("the"):    # remove starting 'the' from artist e.g. the who -> who
+            artist = artist[3:]
+        url = "http://azlyrics.com/lyrics/"+artist+"/"+song_title+".html"
+
+        try:
+            content = urllib.request.urlopen(url).read()
+            soup = BeautifulSoup(content, 'html.parser')
+            lyrics = str(soup)
+            # lyrics lies between up_partition and down_partition
+            up_partition = '<!-- Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->'
+            down_partition = '<!-- MxM banner -->'
+            lyrics = lyrics.split(up_partition)[1]
+            lyrics = lyrics.split(down_partition)[0]
+            lyrics = lyrics.replace('<br/>','').replace('<br>','').replace('</br>','').replace('<i>',' ').replace('</i>','').replace('</div>','').strip()
+            return Response(lyrics)
+        except Exception as e:
+            # return "Exception occurred \n" +str(e)
+            return Response("Error Occurred"+str(e))
+        
+
